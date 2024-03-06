052edf9736f1e9cb35a92affe9108aa258336a4f
Shane McPhillips <smcphillips99@gmail.com>
2024-03-06 15:59:48 -0500

    Fixed access violation and null HWND. WndProc must not return 0, but return to DefWndProc.

--- TestProject/WinMain.cpp
+++ TestProject/WinMain.cpp
@@ -1,28 +1,35 @@
 #include <windows.h>
+
+#define NAMING_STRING_MAX_CHARS 256
+
+WCHAR CLASS_NAME[NAMING_STRING_MAX_CHARS];
+WCHAR WINDOW_TITLE[NAMING_STRING_MAX_CHARS];
  
 
 //Window function for processing incoming messages.
-LRESULT CALLBACK WindowProc(HWND hWnd, UINT uInt, WPARAM wParam, LPARAM lParam);
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uInt, WPARAM wParam, LPARAM lParam);
 
 
 int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
 	//Create WindowClass
 	//Class name for handling
-	const wchar_t CLASS_NAME[] = L"Initial Window Class";
+	wcscpy_s(CLASS_NAME, L"Initial Window Class");
+	
 	//window title
-	const wchar_t WINDOW_TITLE[] = L"The Spartan Engine v-1.0-a";
+	//const wchar_t WINDOW_TITLE[] = L"The Spartan Engine v-1.0-a";
+	wcscpy_s(WINDOW_TITLE, L"The Spartan Engine v-1.0-a");
 
 	//width and height for window
 	const int wnd_width = 1080;
 	const int wnd_height = 920;
 
 	//Define a window structure.
-	WNDCLASSEX wcex;
+	WNDCLASSEX wcex = {0};
 
 	wcex.cbSize = sizeof(WNDCLASSEX); //Defines the size of the wndclass structure.
 	wcex.style = CS_HREDRAW | CS_VREDRAW; //Defines a redraw everytime horizontal / vertical axis are resized.
-	wcex.lpfnWndProc = WindowProc; //Sets the reference to the window message processing func.
+	wcex.lpfnWndProc = WndProc; //Sets the reference to the window message processing func.
 	wcex.cbClsExtra = 0; //Extra bits needed for the window style? No.
 	wcex.cbWndExtra = 0; //Extra bits needed for the window class? No.
 	wcex.hInstance = hInstance; //HWND instance, pass down from WinMain argument defined.
@@ -30,24 +37,41 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 	wcex.hCursor = LoadCursor(NULL, IDC_ARROW); //Standard cursor set.
 	wcex.hbrBackground = (HBRUSH)GetStockObject(NULL_BRUSH); //H-Brush not needed as directx will handle.
 	wcex.lpszMenuName = nullptr; //No menu class structure.
+	wcex.lpszClassName = CLASS_NAME;
 	wcex.hIconSm = LoadIcon(NULL, IDI_APPLICATION); //Standard small icon set.
 
 	//Register the class structure above.
 	RegisterClassEx(&wcex);
 
 	//Create Window
-	HWND hWnd = CreateWindow(CLASS_NAME, WINDOW_TITLE, WS_OVERLAPPEDWINDOW,
+	HWND wnd = CreateWindowEx(NULL, CLASS_NAME, WINDOW_TITLE, WS_OVERLAPPEDWINDOW,
 		CW_USEDEFAULT, CW_USEDEFAULT, wnd_width, wnd_height,
-		nullptr, nullptr, hInstance, nullptr);
+		NULL, NULL, hInstance, NULL);
+
 
 	//Check if window was created and filled in handle.
-	if (hWnd == NULL) {
-		//error handle here
+	if (!wnd) {
+		//error message box
+		MessageBox(NULL, (LPCWSTR)L"There was an error in creating the display window.\nReference: HWND is NULL", (LPCWSTR)L"Error", MB_ICONERROR | MB_OK | MB_DEFBUTTON1);
 		return 0;
 	}
 	
 	//Display the window
-	ShowWindow(hWnd, SW_SHOW);
+	ShowWindow(wnd, SW_SHOW);
+
+	//Handle Messages
+	MSG msg = {0};
+
+	//Make sure we didn't terminate the window.
+	while (msg.message != WM_QUIT) {
+
+		//Using Peek Message since Get Message locks up thread until a message is received.
+		if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE)) {
+			//process message for keystrokes and mouse clicks with translate. Send message to win proc with dispatch.
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
 
 
 
@@ -56,4 +80,8 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 
 
 	return 0;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uInt, WPARAM wParam, LPARAM lParam) {
+	return DefWindowProc(hWnd, uInt, wParam, lParam);
 }
